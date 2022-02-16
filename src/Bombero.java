1b9948af8dfec878ed8074a7aa0d07c8d3616b85
alasadam1b <alasadam1b@gmail.com>
2022-02-17 00:24:39 +0100

    Creo clase Bombero

--- src/Bombero.java
+++ src/Bombero.java
@@ -1,55 +1,86 @@
 import java.util.Random;
 
-public class Bombero {
+/**
+ * @author Adrián Las
+ */
+public class Bombero
+{
 
     private Random random;
     private Posicion[] posiciones;
     private int total;
 
-    public Bombero(int max){
-        // TODO: Bombero
+    public Bombero(int max) {
+        random = new Random();
+        posiciones = new Posicion[max];
+        total = 0;
     }
 
-    public Posicion getPosicion(int i){
-        // TODO: getPosicion
-        return null;
+    public Posicion getPosicion(int i) {
+        if(i < total) return posiciones[i];
+
+        else return null;
     }
 
     public boolean hayBombaEn(int fila, int columna) {
-        // TODO: hayBombaEn
+        for (int i = 0; i < total; i++) {
+            if(posiciones[i].getFila() == fila && posiciones[i].getColumna() == columna) return true;
+        }
         return false;
     }
 
     public void ponerBombas(int numFilas, int numColumnas, int numBombas) {
-        if (numBombas >= numFilas*numColumnas){
+        if(numBombas >= numFilas * numColumnas) {
             todoSonBombas(numFilas, numColumnas);
-        }else if(numBombas >= numFilas * numColumnas / 2){
+        }
+        else if(numBombas >= numFilas * numColumnas / 2) {
             todoSonBombas(numFilas, numColumnas);
             eliminarBombas(numFilas * numColumnas - numBombas);
-        }else{
+        }
+        else {
             ponerAlgunasBombas(numFilas, numColumnas, numBombas);
         }
     }
 
     private void ponerAlgunasBombas(int numFilas, int numColumnas, int numBombas) {
-        // TODO: ponerAlgunasBombas
+        for (int i = 0; i < numBombas; ) {
+            int afil = random.nextInt(numFilas);
+            int acol = random.nextInt(numColumnas);
+            if(!hayBombaEn(afil, acol)) {
+                posiciones[total] = new Posicion(afil, acol);
+                total++;
+                i++;
+            }
+        }
     }
 
-    private void todoSonBombas(int numFilas, int numColumnas){
-        // TODO: todoSonBombas
+    private void todoSonBombas(int numFilas, int numColumnas) {
+        for (int f = 0; f < numFilas; f++) {
+            for (int c = 0; c < numColumnas; c++) {
+                posiciones[total] = new Posicion(f, c);
+                total++;
+            }
+        }
     }
 
-    private void eliminarBombas(int n){
-        // TODO: eliminarBombas
+    private void eliminarBombas(int n) {
+        for (int i = 0; i < n; i++) {
+            eliminarBomba();
+        }
     }
 
-    private void eliminarBomba(){
-        // TODO: eliminarBomba
+    private void eliminarBomba() {
+        if(total > 0) {
+            int num = random.nextInt(total);
+            System.arraycopy(posiciones, num + 1, posiciones, num, total - num - 1);
+            posiciones[total - 1] = null;
+            total--;
+        }
     }
 
     public String toString() {
         String s = "[";
-        if (total > 0){
+        if(total > 0) {
             s += posiciones[0].toString();
         }
         for (int i = 1; i < total; i++) {
@@ -59,5 +90,4 @@ public class Bombero {
         s += "]";
         return s;
     }
-
 }
