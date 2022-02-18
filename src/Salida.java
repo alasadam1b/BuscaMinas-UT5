09a371aab7c761b096bddf0ae87f8d34568135a9
alasadam1b <alasadam1b@gmail.com>
2022-02-18 01:12:52 +0100

    Creo clases Tablero y Salida

--- src/Salida.java
+++ src/Salida.java
@@ -1,7 +1,43 @@
-public class Salida {
+/**
+ * @author Adrián Las
+ */
+public class Salida
+{
 
     public void pinta(int[][] matriz) {
-        // TODO: pinta
-    }
+        StringBuilder lineaFormateada = new StringBuilder("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + String.format("%4c", ' '));
+        int numf = 0;
+        String separador = "     |";
+        for (int i = 0; i < matriz[0].length; i++) {
+            separador += "---|";
+        }
+        separador += "\n";
+        for (int f = 0; f < matriz.length; f++) {
+            if(f == 0) {
+                int numc = 0;
+                for (int cs = 0; cs < matriz[0].length; cs++) {
+                    lineaFormateada.append(String.format("%4d", numc));
+                    numc++;
+                }
+                lineaFormateada.append("\n");
+            }
+            lineaFormateada.append(separador);
+            for (int c = 0; c < matriz[0].length; c++) {
+                if(c == 0) {
+                    lineaFormateada.append(String.format("%4d%2c", numf, '|'));
+                    numf++;
+                }
 
+                if(matriz[f][c] == V.TAPADO) {
+                    lineaFormateada.append(String.format("%2c%2c", ' ', '|'));
+                }
+                else if(matriz[f][c] >= 0 && matriz[f][c] <= 8) {
+                    lineaFormateada.append(String.format("%2d%2c", matriz[f][c], '|'));
+                }
+                else lineaFormateada.append(String.format("%2c%2c", 'X', '|'));
+            }
+            lineaFormateada.append("\n");
+        }
+        System.out.println(lineaFormateada.toString());
+    }
 }
--- src/Tablero.java
+++ src/Tablero.java
@@ -1,4 +1,8 @@
-public class Tablero {
+/**
+ * @author Adrián Las
+ */
+public class Tablero
+{
 
     private int numFilas;
     private int numColumnas;
@@ -8,36 +12,58 @@ public class Tablero {
     private int descubiertos;
     private boolean bombasDescubiertas;
 
-    public Tablero(int numFilas, int numColumnas, int numBombas){
+    public Tablero(int numFilas, int numColumnas, int numBombas) {
         this.numFilas = numFilas;
         this.numColumnas = numColumnas;
-        this.numBombas = Math.min(numBombas, numFilas*numColumnas);
+        this.numBombas = Math.min(numBombas, numFilas * numColumnas);
         matriz = new int[numFilas][numColumnas];
         limpiarTablero();
-        bombero = new Bombero(numFilas*numColumnas);
+        bombero = new Bombero(numFilas * numColumnas);
         bombero.ponerBombas(numFilas, numColumnas, numBombas);
         descubiertos = 0;
         bombasDescubiertas = false;
     }
 
-    public int[][] getMatriz(){
+    public int[][] getMatriz() {
         return matriz;
     }
 
-    private void limpiarTablero(){
-        // TODO: limpiarTablero
+    private void limpiarTablero() {
+        for (int f = 0; f < numFilas; f++) {
+            for (int c = 0; c < numColumnas; c++) {
+                matriz[f][c] = V.TAPADO;
+            }
+        }
     }
 
     public boolean hayBombaEn(int fila, int columna) {
-        return bombero.hayBombaEn(fila,columna);
+        return bombero.hayBombaEn(fila, columna);
+    }
+
+    public boolean dentroLimites(int f, int c) {
+        return f >= 0 && f < matriz.length && c >= 0 && c < matriz[0].length;
     }
 
     public void calculaBombasVecinas(int fila, int columna) {
-        // TODO: calculaBombasVecinas
+        int calculo = 0;
+        if(dentroLimites(fila, columna) && bombero.hayBombaEn(fila, columna + 1)) calculo++;
+        if(dentroLimites(fila, columna) && bombero.hayBombaEn(fila, columna - 1)) calculo++;
+        if(dentroLimites(fila, columna) && bombero.hayBombaEn(fila + 1, columna)) calculo++;
+        if(dentroLimites(fila, columna) && bombero.hayBombaEn(fila - 1, columna)) calculo++;
+        if(dentroLimites(fila, columna) && bombero.hayBombaEn(fila + 1, columna + 1)) calculo++;
+        if(dentroLimites(fila, columna) && bombero.hayBombaEn(fila - 1, columna - 1)) calculo++;
+        if(dentroLimites(fila, columna) && bombero.hayBombaEn(fila - 1, columna + 1)) calculo++;
+        if(dentroLimites(fila, columna) && bombero.hayBombaEn(fila + 1, columna - 1)) calculo++;
+
+        if(matriz[fila][columna] == V.TAPADO) descubiertos++;
+        matriz[fila][columna] = calculo;
     }
 
     public void descubreBombas() {
-        // TODO: descubreBombas
+        for (int i = 0; i < numBombas; i++) {
+            matriz[bombero.getPosicion(i).getFila()][bombero.getPosicion(i).getColumna()] = V.BOMBA;
+        }
+        bombasDescubiertas = true;
     }
 
     public boolean estanDescubiertasTodasLasNoBombas() {
